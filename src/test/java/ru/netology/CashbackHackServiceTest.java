f536d5038b5cdb78c459005f185b3e7c85a4f630
Vladimir <vladimir.zhevagin@yandex.ru>
2021-04-20 18:02:38 +0300

    тесты

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,8 +1,41 @@
 package ru.netology;
 
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
+    @Test
+    void shouldCashbackHackLess1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
 
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void shouldCashbackHackMore1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void shouldCashbackHack1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
 
 }
