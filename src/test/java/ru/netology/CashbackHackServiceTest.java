98a1ad003a7e4000b9b0c6afc2e62601268c50df
Vladimir <vladimir.zhevagin@yandex.ru>
2021-04-20 21:38:01 +0300

    junit-platform2

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -33,12 +33,28 @@ public class CashbackHackServiceTest {
         int amount = 1000;
 
         int actual = service.remain(amount);
-        int expected = 1000;
+        int expected = 0;
 
         assertEquals(expected, actual);
     }
 
+    @org.junit.jupiter.api.Test
+    void shouldJupiterLess1000(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
 
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldJupiterMore1000(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
 
+        int actual = service.remain(amount);
+        int expected = 900;
+        assertEquals(expected, actual);
+    }
 
 }
