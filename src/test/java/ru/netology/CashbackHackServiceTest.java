4bdc8520d6f4592ee1885a77c10931a7e1710e82
Vladimir <vladimir.zhevagin@yandex.ru>
2021-05-05 18:47:19 +0300

    изменено

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -27,14 +27,14 @@ public class CashbackHackServiceTest {
         assertEquals(expected, actual);
     }
 
-//    @Test
-//    public void shouldCashbackHack1000() {
-//        CashbackHackService service = new CashbackHackService();
-//        int amount = 1000;
-//
-//        int actual = service.remain(amount);
-//        int expected = 0;
-//
-//        assertEquals(expected, actual);
-//    }
+    @Test
+    public void shouldCashbackHack1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
 }
